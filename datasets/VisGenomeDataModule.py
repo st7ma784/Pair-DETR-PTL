b589252e657380cc18a90a8e67950ca59e3c60d0
steve <you@example.com>
2023-07-26 10:29:34 +0100

    fix eval

---
+++ datasets/VisGenomeDataModule.py
@@ -0,0 +1,210 @@
+#So I think you just need to update that line to:
+import torchvision
+#torchvision.disable_beta_transforms_warning()
+import torchvision.transforms.v2 as transforms
+#from torchvision.transforms import v2 as transforms
+
+import torch
+from transformers import CLIPTokenizer
+import os
+import pytorch_lightning as pl
+from torch.utils.data import Dataset,IterableDataset
+from datasets import load_dataset
+from PIL import Image
+import random
+import requests
+from io import BytesIO
+from torchvision import datapoints
+prep=transforms.Compose([
+        transforms.Resize(224, interpolation=Image.NEAREST),
+        transforms.CenterCrop(224),
+        #Note: the standard  lambda function here is not supported by pytorch lightning
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        ])
+
+class VisGenomeIterDataset(IterableDataset):
+    def __init__(self,split="train",dir="HF",T=prep,tokenizer=None):
+        #print('Loading COCO dataset')
+        self.data=load_dataset("visual_genome", "relationships_v1.2.0",streaming=True,cache_dir=dir)[split]
+        print("got datast")
+        self.T=T
+        self.tokenizer=tokenizer
+        if tokenizer is None:
+            self.tokenizer=CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32",cache_dir=dir) 
+        self.tokenize=lambda x:self.tokenizer(x,return_tensors="pt",padding="max_length", truncation=True,max_length=77)['input_ids']
+        self.data =self.data.map(self.process,remove_columns=['image_id' , 'image','relationships','width', 'height', 'coco_id', 'flickr_id','url' ])
+
+    def process(self,item):
+        caption=""
+        if len(item["relationships"])==0:
+            caption=" an image with no objects interacting"
+        else:
+            r=random.choice(item["relationships"])
+            caption=" ".join(["a",r["subject"]["names"][0],r["predicate"],r["object"]["names"][0]])
+        r=self.tokenize(caption)
+        try:
+            return {"img":prep(item["image"]),"relation":r}
+        except FileNotFoundError as e:
+            response = requests.get(item["url"])
+            img = Image.open(BytesIO(response.content))
+            print("failed : {}".format(item["image"]))
+            return {"img":prep(img),"relation":r}
+    def __len__(self):
+        return 108077
+    def __iter__(self):
+        # item= self.data.next()
+        # r=random.choice(item["relationships"])
+        # r=self.tokenize(" ".join(["a",r["subject"]["names"][0],r["predicate"],r["object"]["names"][0]]), return_tensors="pt",padding="max_length", truncation=True,max_length=77)['input_ids'])
+        # out= {"img":prep(item["image"]),"relation":r}
+        #print(self.data.__dir__())
+        return self.data.__iter__()
+
+    
+
+class VisGenomeDataset(Dataset):
+    def __init__(self, split="train",dir="HF",T=prep,tokenizer=None):
+        #print('Loading COCO dataset')
+        self.data=load_dataset("visual_genome", "relationships_v1.2.0",streaming=False,cache_dir=dir)[split]
+        self.T=T
+        self.tokenizer=tokenizer
+        if tokenizer is None:
+            self.tokenizer=CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32",cache_dir=dir) 
+        self.tokenize=lambda x:self.tokenizer(x,return_tensors="pt",padding="max_length", truncation=True,max_length=77)['input_ids']
+        # self.data =self.data.map(self.process,remove_columns=['image_id' , 'image','relationships','width', 'height', 'coco_id', 'flickr_id','url' ],num_proc=8).filter(lambda example: example is not None)
+        
+    def process(self,item):
+        caption=" "
+        if len(item["relationships"])==0:
+            caption=" an image with no objects interacting"
+        else:
+            r=random.choice(item["relationships"])
+            caption=" ".join(["a",r["subject"]["names"][0],r["predicate"],r["object"]["names"][0]])
+        r=self.tokenize(caption)
+        #print(item)
+        try:
+            return {"img":prep(item["image"]),"relation":r}
+        except FileNotFoundError as e:
+            response = requests.get(item["url"])
+            img = Image.open(BytesIO(response.content))
+            #print("failed : {}".format(item["image"]))
+            return {"img":prep(img),"relation":r}
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self,idx):
+        return self.process(self.data.__getitem__(idx))
+
+class VisGenomeDatasetBigBoxes(VisGenomeDataset):
+    def process(self,item):
+        if len(item["relationships"])==0:
+            return None
+        #this is probably awful,  Why not look at the Pairwise DETR for how it does contrastive loss on multiple objects? 
+        r=random.choice(item["relationships"])
+        #s is the r["subject"] box
+        s=datapoints.BoundingBox([r["subject"]["x"],r["subject"]["y"],r["subject"]["w"],r["subject"]["h"]], format=datapoints.BoundingBoxFormat.XYWH, spatial_size=[item["width"],item["height"]])
+        o=datapoints.BoundingBox([r["object"]["x"],r["object"]["y"],r["object"]["w"],r["object"]["h"]], format=datapoints.BoundingBoxFormat.XYWH, spatial_size=[item["width"],item["height"]])
+
+        
+        l=datapoints.BoundingBox([min(r["object"]["x"],r["subject"]["x"]),
+                                 min(r["object"]["y"],r["subject"]["y"]),
+                                 max(r["object"]["w"],r["subject"]["w"]),
+                                 max(r["object"]["h"],r["subject"]["h"])], format=datapoints.BoundingBoxFormat.XYWH, spatial_size=[item["width"],item["height"]])
+        r=self.tokenize(" ".join(["a",r["subject"]["names"][0],r["predicate"],r["object"]["names"][0]]))
+
+        img=item["image"]
+        try:
+            i,t=prep(img,{"boxes":[s,o,l]})
+            return {"img":i,"relation":r,"targets":t}
+        except FileNotFoundError as e:
+            response = requests.get(item["url"])
+            img = Image.open(BytesIO(response.content))
+            #print("failed : {}".format(item["image"]))
+            i,t=prep(img,{"boxes":[s,o,l]})
+            return {"img":i,"targets":t,"relation":r}
+            
+class VisGenomeDatasetIterBigBoxes(VisGenomeDataset):
+    def process(self,item):
+
+        if len(item["relationships"])==0:
+            return None
+        r=random.choice(item["relationships"])
+        #s is the r["subject"] box
+        s=datapoints.BoundingBox([r["subject"]["x"],r["subject"]["y"],r["subject"]["w"],r["subject"]["h"]], format=datapoints.BoundingBoxFormat.XYWH, spatial_size=[item["width"],item["height"]])
+        o=datapoints.BoundingBox([r["object"]["x"],r["object"]["y"],r["object"]["w"],r["object"]["h"]], format=datapoints.BoundingBoxFormat.XYWH, spatial_size=[item["width"],item["height"]])
+
+        
+        l=datapoints.BoundingBox([min(r["object"]["x"],r["subject"]["x"]),
+                                 min(r["object"]["y"],r["subject"]["y"]),
+                                 max(r["object"]["w"],r["subject"]["w"]),
+                                 max(r["object"]["h"],r["subject"]["h"])], format=datapoints.BoundingBoxFormat.XYWH, spatial_size=[item["width"],item["height"]])
+        r=self.tokenize(" ".join(["a",r["subject"]["names"][0],r["predicate"],r["object"]["names"][0]]))
+        img=item["image"]
+
+        try:
+            i,t=prep(img,{"boxes":[s,o,l]})
+            return {"img":i,"relation":r,"targets":t}
+        except FileNotFoundError as e:
+            response = requests.get(item["url"])
+            img = Image.open(BytesIO(response.content))
+            #print("failed : {}".format(item["image"]))
+            i,t=prep(img,{"boxes":[s,o,l]})
+            return {"img":i,"targets":t,"relation":r}
+# Dataset
+class VisGenomeDataModule(pl.LightningDataModule):
+
+    def __init__(self, Cache_dir='.', T=prep, batch_size=256,stream=False, fullBoxes=False):
+        super().__init__()
+        self.data_dir = Cache_dir
+        self.batch_size = batch_size
+        self.stream=stream
+        self.tokenizer=CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32") 
+
+        if fullBoxes:
+            self.dataConstructor=VisGenomeDatasetBigBoxes
+            if self.stream:
+                self.dataConstructor=VisGenomeDatasetIterBigBoxes
+        else:
+            self.dataConstructor=VisGenomeDataset
+            if self.stream:
+                self.dataConstructor=VisGenomeIterDataset
+        self.T=T
+
+    def train_dataloader(self, B=None):
+        if B is None:
+            B=self.batch_size 
+        return torch.utils.data.DataLoader(self.train, batch_size=B, shuffle=not self.stream, num_workers=4, prefetch_factor=3, pin_memory=True,drop_last=True)
+    def val_dataloader(self, B=None):
+        if B is None:
+            B=self.batch_size
+       
+        return torch.utils.data.DataLoader(self.val, batch_size=B, shuffle=not self.stream, num_workers=1, prefetch_factor=1, pin_memory=True,drop_last=True)
+    def test_dataloader(self,B=None):
+        if B is None:
+            B=self.batch_size
+
+
+        return torch.utils.data.DataLoader(self.test, batch_size=B, shuffle=not self.stream, num_workers=4, prefetch_factor=4, pin_memory=True,drop_last=True)
+
+    def setup(self, stage=None):
+        '''called on each GPU separately - stage defines if we are at fit or test step'''
+        #print("Entered COCO datasetup")
+        
+        #if stage == 'fit' or stage is None:
+        self.train=self.dataConstructor(T=self.T,dir=self.data_dir,split="train",tokenizer=self.tokenizer)
+    
+
+
+
+if __name__ == "__main__":
+    #run this to test the dataloader
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--Cache_dir', type=str, default='.', help='path to download and cache data')
+    dir=os.path.join(parser.parse_args().Cache_dir,"data")
+    dm =VisGenomeDataModule(Cache_dir="HF",batch_size=3)
+    dm.prepare_data()
+    dm.setup()
+    train_loader = dm.train_dataloader()
+    for batch in train_loader:
+        print(batch)
+        break
