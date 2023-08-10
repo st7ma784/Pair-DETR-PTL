fc1fa0baaff5b50680f459f343efb102b8850e26
steve <you@example.com>
2023-08-10 10:11:43 +0100

    fx

--- data/VisGenomeDataModuleSelfDETIC.py
+++ data/VisGenomeDataModuleSelfDETIC.py
@@ -224,7 +224,7 @@ def DETICprocess(self,item):
         #convert to tensors
         print("obj_bboxes",obj_bboxes.shape)
         print("found_boxes",found_boxes)
-        found_boxes=torch.tensor(found_boxes)
+        #found_boxes=torch.tensor(found_boxes) #######################################
         annotation_to_output_ious=torchvision.ops.box_iou(obj_bboxes,found_boxes)
         #find max iou +_idx for each annotation 
         max_ious,max_idx=torch.max(annotation_to_output_ious,dim=1)
