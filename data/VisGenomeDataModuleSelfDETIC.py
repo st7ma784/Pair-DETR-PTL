902789de88f8121d7579aa406c3c26ed4d274c05
steve <you@example.com>
2023-08-10 11:06:04 +0100

    fc

--- data/VisGenomeDataModuleSelfDETIC.py
+++ data/VisGenomeDataModuleSelfDETIC.py
@@ -247,9 +247,9 @@ def DETICprocess(self,item):
                                 min(r["object"]["y"],r["subject"]["y"]), # these find the top left corner
                                 max(r["object"]["x"],r["subject"]["x"])-min(r["object"]["x"],r["subject"]["x"]) +max(r["object"]["w"],r["subject"]["w"]), # find the bottom right corner with max of x ys and add the whs.  
                             max(r["object"]["y"],r["subject"]["y"])-min(r["object"]["y"],r["subject"]["y"])+ max(r["object"]["h"],r["subject"]["h"])]).unsqueeze(0)
-        print("original_bbox",original_bbox)
-        print("object_actual_bbox_from_mask",object_actual_bbox_from_mask)
-        print("Comparison of boxes: ", torchvision.ops.box_iou(original_bbox,object_actual_bbox_from_mask).item())
+        print("original_bbox",original_bbox/divisor)
+        print("object_actual_bbox_from_mask",object_actual_bbox_from_mask/divisor)
+        print("Comparison of boxes: ", torchvision.ops.box_iou(original_bbox/divisor,object_actual_bbox_from_mask/divisor).item())
 
 
         out.append({"boxes":object_actual_bbox_from_mask,
