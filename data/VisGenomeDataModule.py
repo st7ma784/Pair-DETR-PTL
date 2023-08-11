7c13c50e6a855531ec60ad15eccf1eca48ac4769
steve <you@example.com>
2023-08-11 23:47:21 +0100

    FC

--- data/VisGenomeDataModule.py
+++ data/VisGenomeDataModule.py
@@ -100,7 +100,7 @@ class VisGenomeDataset(Dataset):
             response = requests.get(item["url"])
             img,boxes = prep(Image.open(BytesIO(response.content)),{"boxes":torch.stack(objects+subjects)})
         print(boxes)
-        return {"img":img,"relation":torch.stack(captions),"objects":torch.stack(boxes[:(len(boxes)//2)]),"subjects":torch.stack(boxes[(len(boxes)//2):]), "obj_classes":torch.stack([self.tokenize(x) for x in obj_classes]),"subj_classes":torch.stack([self.tokenize(x) for x in subj_classes])}
+        return {"img":img,"relation":torch.stack(captions),"objects":torch.stack(boxes["boxes"][:(len(boxes["boxes"])//2)]),"subjects":torch.stack(boxes["boxes"][(len(boxes["boxes"])//2):]), "obj_classes":torch.stack([self.tokenize(x) for x in obj_classes]),"subj_classes":torch.stack([self.tokenize(x) for x in subj_classes])}
         
     def __len__(self):
         return len(self.data)
