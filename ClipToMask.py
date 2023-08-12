de89b39447e8df360c50e532d6965c6e2689fab0
steve <you@example.com>
2023-08-12 17:46:34 +0100

    f

--- ClipToMask.py
+++ ClipToMask.py
@@ -145,7 +145,7 @@ class Exp3ClipToVisGenomeMask(Exp2CLIPtoCOCOMask):
         metadata.thing_classes = self.detic.model.roi_heads.num_classes
         classifier=torch.cat([obj_classes_encodings,subj_classes_encodings],dim=0)
         zs_weight = torch.cat([classifier, classifier.new_zeros((classifier.shape[0], 1))], dim=1) # D x (C + 1)
-        if self.detic.roi_heads.box_predictor[0].cls_score.norm_weight:
+        if self.detic.model.roi_heads.box_predictor[0].cls_score.norm_weight:
             zs_weight = torch.nn.functional.normalize(zs_weight, p=2, dim=0)
         zs_weight = zs_weight.to(self.cfg.MODEL.DEVICE)
         for k in range(len(self.detic.model.roi_heads.box_predictor)):
