faa721da6e1db3bb7523a6fc3aa28b67d471a603
steve <you@example.com>
2023-08-12 18:58:17 +0100

    fx

--- ClipToMask.py
+++ ClipToMask.py
@@ -171,8 +171,11 @@ class Exp3ClipToVisGenomeMask(Exp2CLIPtoCOCOMask):
         #   print(self.detic.proposal_generator)
 
             #we#re going to create input with as objects that input.tensor=img and input.image_sizes=img.shape[1:].unsqueeze(0).repeat(img.shape[0],1,1)
-            inputs=TensorWrapper(img)
+            #inputs=TensorWrapper(img)
             img.image_sizes=torch.tensor(img.shape[1:]).unsqueeze(0).repeat(img.shape[0],1)
+            #apparently Tensor obj has no attribute image_sizes
+            setattr(img,"image_sizes",torch.tensor(img.shape[1:]).unsqueeze(0).repeat(img.shape[0],1))
+
             proposals, _ = self.detic.model.proposal_generator(img, features,)
             outputs, _ = self.detic.model.roi_heads(img, features, proposals)
         #     print("outputs",outputs.keys())
