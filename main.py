d738a5d5686078f2667d994f60a48469b33e92e0
steve <you@example.com>
2023-08-17 15:56:03 +0100

    fx

--- main.py
+++ main.py
@@ -462,10 +462,10 @@ class VisGenomeModule(PairDETR):
         batched=[{k : torch.stack([t[k] for t in ts],dim=0) for k in ts[0].keys()} for ts in targetsets ]
         
         return img, batched , dict(enumerate(classencodings)), masks_per_image.squeeze(1), batch_idx, (tgt_ids,tgt_bbox,masks_per_caption.squeeze(),tgt_sizes)
-    def on_validation_epoch_start(self):
+    def validation_epoch_start(self):
         #move the model to the device
         self.detic.model.to(self.device)
-    def on_train_epoch_start(self):
+    def train_epoch_start(self):
         #move the model to the device
         self.detic.model.to(self.device)
     def training_step(self,batch,batch_idx):
