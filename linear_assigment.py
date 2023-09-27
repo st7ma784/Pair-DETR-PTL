c2a7b7dfd92b663c592f4626f463ca8f1486214d
steve <you@example.com>
2023-09-27 11:32:01 +0100

    update all

--- linear_assigment.py
+++ linear_assigment.py
@@ -1,5 +1,4 @@
 import torch
-
 def assign2D(C):
     #convert to pytorch the function in linear_assigment.m
 
@@ -10,130 +9,149 @@ def assign2D(C):
     if didFlip:
         C=C.transpose(1,0)
         numRow,numCol=C.shape
-    C=C.sigmoid()
-    #These store the assignment as it is made.
-    col4row=torch.zeros(numRow,dtype=torch.int64)
-    row4col=torch.zeros(numCol,dtype=torch.int64)
-    u=torch.zeros(numCol)#The dual variable for the columns
-    v=torch.zeros(numRow)#The dual variable for the rows.
+    col4row=torch.zeros(numRow,dtype=torch.int64,device=C.device)
+    row4col=torch.zeros(numCol,dtype=torch.int64,device=C.device)
+    u=torch.zeros(numCol,device=C.device)#The dual variable for the columns
+    v=torch.zeros(numRow,device=C.device)#The dual variable for the rows.
     
     #Initially, none of the columns are assigned.
     for curUnassCol in range(numCol):       
         #This finds the shortest augmenting path starting at k and returns
         #the last node in the path.
         [sink,pred,u,v]=ShortestPath(curUnassCol,u,v,C,col4row,row4col)
-        assert sink!=-1,"The problem is not feasible."
         #We have to remove node k from those that must be assigned.
-        i=-1
-        while(i!=curUnassCol):
-            i=pred[sink]
-            col4row[sink]=i
-            h=row4col[i]
-            row4col[i]=sink
-            sink=h   
-    
+        # i=-1
+        
+        # while(i!=curUnassCol):
+        #     i=pred[sink]
+        #     print("repeating for across \n {}  \n until we reach {}, currently on {}".format(pred,curUnassCol,i))
+
+        #     col4row[sink]=i
+        #     h=row4col[i]
+        #     row4col[i]=sink
+        #     sink=h   
+        col4row[sink]=pred[sink]
+        row4col[pred[sink]]=sink
     if didFlip:
-        temp=row4col
+        temp=row4col.clone()
         row4col=col4row
         col4row=temp
-        
         temp=u
         u=v
         v=temp
-    return col4row,row4col#,u,v
-
-def ShortestPath(curUnassCol,u,v,C,col4row,row4col):
+    return row4col#,u,vcol4row
+@torch.jit.script
+def ShortestPath(curUnassCol:int ,u: torch.Tensor ,v:torch.Tensor,C:torch.Tensor,col4row:torch.Tensor,row4col:torch.Tensor): #-> Tuple[int,torch.Tensor,torch.Tensor,torch.Tensor,torch.Tensor]:
     numRow,numCol=C.shape
     #numCol=size(C,2);
-    pred=torch.zeros(numRow,dtype=torch.int64)
-    
-    #Initially, none of the rows and columns have been scanned.
-    #This will store a 1 in every column that has been scanned.
-    ScannedCols=torch.zeros(numCol)
-    
-    #This will store a 1 in every row that has been scanned.
-    ScannedRow=torch.zeros(numRow)
-    Row2Scan=[*range(0,numRow)]
-    numRow2Scan=numRow
-    
+    pred=torch.zeros(numRow,dtype=torch.int64,device=C.device)
+    ScannedCols=torch.zeros(numCol,dtype=torch.bool,device=C.device)
+    ScannedRow=torch.zeros(numRow,dtype=torch.bool,device=C.device)
+    Row2Scan=torch.arange(0,numRow,device=C.device)
     sink=0
-    delta=0
+    delta=torch.tensor(0)
     curCol=int(curUnassCol)
     #set array of inf
-    shortestPathCost=torch.full((numRow,),100)    
-    while sink==0:
+    shortestPathCost=torch.full((numRow,),100.0,device=C.device)    
+    while torch.all(ScannedRow.nonzero()) and sink==0:
         #Mark the current column as having been visited.
         ScannedCols[curCol]=1
-        
-        #Scan all of the rows that have not already been scanned.
-        minVal=torch.tensor(100,dtype=torch.float64)
-        for i,curRowScan in enumerate(Row2Scan):
-            curRow=int(curRowScan)
-            
-            reducedCost=delta+C[curRow,curCol]-u[curCol]-v[curRow]
-            if reducedCost<shortestPathCost[curRow]:
-                pred[curRow]=curCol
-                shortestPathCost[curRow]=reducedCost
-            
-            
-            #%Find the minimum unassigned row that was scanned.
-            if shortestPathCost[curRow]<minVal:
-                minVal=shortestPathCost[curRow]
-                closestRowScan=i
-            
-        
-
-        if minVal.isinf():
-           #If the minimum cost row is not finite, then the problem is
-           #not feasible
-           return (-1,pred,u,v)
-        
-        
-        closestRow=Row2Scan[closestRowScan]
-        
-        #%Add the row to the list of scanned rows and delete it from
-        #%the list of rows to scan.
+        minVal=torch.tensor(float('inf'),device=C.device)
+        reducedCosts=C[:,curCol] -u[curCol] -v
+        reducedCosts=reducedCosts[ScannedRow==0]
+        smallestReducedCost_v,smallestReducedCost_i=torch.min(reducedCosts.unsqueeze(0),dim=1)
+        shortestPathCost[ScannedRow==0][smallestReducedCost_i]=smallestReducedCost_v
+        pred[ScannedRow==0][smallestReducedCost_i]=curCol
+        minVal=torch.min(smallestReducedCost_v,minVal)   
+        closestRow=Row2Scan[ScannedRow==0][smallestReducedCost_i]
         ScannedRow[closestRow]=1
-        numRow2Scan=numRow2Scan-1
-        Row2Scan.remove(Row2Scan[closestRowScan])
-        
-        delta=shortestPathCost[closestRow]
+        # Row2Scan=Row2Scan[ScannedRow==0]
+        ##extra
+        delta=smallestReducedCost_v
         
         #If we have reached an unassigned column.
         if col4row[closestRow]==0:
-            sink=closestRow
-            curCol=int(col4row[closestRow])
-        
-    
-    
-    #%Dual Update Step
-    
-    #%#Update the first column in the augmenting path.
-    u[curUnassCol]=u[curUnassCol]+delta
-    #%Update the rest of the columns in the augmenting path.
-    #print("curUnassCol",curUnassCol)
-    #print("ScannedCols {} {}".format(ScannedCols.shape,ScannedCols))# 1,1
-    sel= ScannedCols!=0 #find(ScannedCols~=0)
-    #print("sel {} {}".format(sel.shape,sel))# 1,1 
-    sel[curUnassCol]=torch.zeros([])
-    #print("u {} {}".format(u.shape ,u))# 1,1
-    #print(row4col.shape)# 1)
+            sink=closestRow.item()
+            curCol=col4row[closestRow].item()
+    ###not sure if this is needed
+    u[curUnassCol]=u[curUnassCol]+delta.item()
+    sel= ScannedCols
+    sel[curUnassCol]=0
     u[sel]=u[sel] -shortestPathCost[row4col[sel]]+delta
-    
-    #%Update the scanned rows in the augmenting path.
-    #in torch we can't do ScannedRow~=0 - so we do this
-    sel=sel.nonzero()
+    sel=ScannedRow
     v[sel]=v[sel]-delta+shortestPathCost[sel]
-
     return (sink,pred,u,v)
 
+
+def myLinearSum(C):
+
+       
+    numRow,numCol=C.shape
+    
+    # didFlip=numCol>numRow
+    # if didFlip:
+    #     C=C.transpose(1,0)
+    #     numRow,numCol=C.shape
+    
+    col4row=torch.zeros(numRow,dtype=torch.int64,device=C.device)
+    row4col=torch.zeros(numCol,dtype=torch.int64,device=C.device)
+    u=torch.zeros(numCol,device=C.device)#The dual variable for the columns
+    v=torch.zeros(numRow,device=C.device)#The dual variable for the rows.
+    
+    #Initially, none of the columns are assigned.
+    for curCol in range(numCol):       
+        pred=torch.zeros(numRow,dtype=torch.int64,device=C.device)
+        #ScannedCols=torch.zeros(numCol,dtype=torch.bool,device=C.device)
+        ScannedRow=torch.zeros(numRow,dtype=torch.bool,device=C.device)
+        Row2Scan=torch.arange(0,numRow,device=C.device)
+        sink=torch.tensor(0,device=C.device)
+        #curCol=int(curUnassCol)
+        #set array of inf
+        #minVal=torch.tensor(float('inf'),device=C.device)
+        ScannedCols=torch.zeros(numCol,dtype=torch.bool,device=C.device)
+        shortestPathCost=torch.full((numRow,),float('inf'),device=C.device)    
+        while torch.all(ScannedRow.nonzero()) and torch.all(sink==0):
+            reducedCosts=C[ScannedRow==0][curCol] # extra ? #-u[curCol]-v[Row2Scan[ScannedRow==0]]
+            smallestReducedCost_i=torch.argmin(reducedCosts)
+
+            smallestReducedCost_v=torch.min(reducedCosts)
+            shortestPathCost[ScannedRow==0][smallestReducedCost_i]=smallestReducedCost_v
+            pred[ScannedRow==0][smallestReducedCost_i]=curCol
+            #minVal=torch.min(smallestReducedCost_v,minVal)
+            
+            ####surely this is wrong??! or at least REALLY inefficient
+            closestRowScan=Row2Scan[ScannedRow==0][smallestReducedCost_i]      
+            closestRow=Row2Scan[ScannedRow==0][closestRowScan]
+            ScannedRow[closestRow]=1
+            Row2Scan=Row2Scan[ScannedRow==0]
+            delta=smallestReducedCost_v
+            if col4row[closestRow]==0:
+                sink=closestRow
+                curCol=col4row[closestRow]
+        u[curCol]=u[curCol]+delta
+        u[curCol]=u[curCol]+delta
+        sel= ScannedCols 
+        sel[curCol]=0
+        u[sel]=u[sel] -shortestPathCost[row4col[sel]]+delta
+        sel=ScannedRow
+        v[sel]=v[sel]-delta+shortestPathCost[sel]
+        col4row[sink]=pred[sink]
+        row4col[pred[sink]]=sink
+    # if didFlip:
+    #     temp=row4col.clone()
+    #     row4col=col4row
+    #     col4row=temp
+    #     temp=u
+    #     u=v
+    #     v=temp
+    return row4col#,u,vcol4row
 
 
 
 B=20
 n_queries=80
 chunks=torch.randint(1,4,(B,))
-cost_Array=torch.rand((chunks.sum(),B*n_queries))
+cost_Array=torch.rand((chunks.sum(),B*n_queries)).cuda().view(B, n_queries, -1)
 f=torch.arange(B)
 g=torch.stack([f,f,f])
 h=g.flatten()
@@ -150,11 +168,31 @@ h=g.flatten()
 
 #     #this is the same as the above and is faster?
 
-C = cost_Array.view(B, n_queries, -1).cpu()
-
 from scipy.optimize import linear_sum_assignment
-indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(chunks.tolist(), -1))]
+
+with torch.autograd.profiler.profile() as prof:
+    B = cost_Array.clone().cpu()
+
+    indices = [linear_sum_assignment(c[i]) for i, c in enumerate(B.split(chunks.tolist(), -1))]
+
+print(prof.key_averages().table(sort_by="self_cpu_time_total"))
+
+
+C = cost_Array.clone()
+
 myindices = [assign2D(c[i]) for i, c in enumerate(C.split(chunks.tolist(), -1))]
+myindices = [myLinearSum(c[i]) for i, c in enumerate(C.split(chunks.tolist(), -1))]
+
+with torch.autograd.profiler.profile() as prof:
+
+    C = cost_Array.clone()
+    myindices = [assign2D(c[i]) for i, c in enumerate(C.split(chunks.tolist(), -1))]
+print(prof.key_averages().table(sort_by="self_cuda_time_total"))
+with torch.autograd.profiler.profile() as prof:
+
+    C = cost_Array.clone()
+    myindices = [myLinearSum(c[i]) for i, c in enumerate(C.split(chunks.tolist(), -1))]
+print(prof.key_averages().table(sort_by="self_cuda_time_total"))
 
 print(indices)
 print(myindices)
