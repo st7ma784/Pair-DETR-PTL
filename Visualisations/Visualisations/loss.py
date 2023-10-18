5284896ad112cf1752b8e8d984e861e8ed92a91b
steve <you@example.com>
2023-10-18 23:59:51 +0100

    fx

--- Visualisations/Visualisations/loss.py
+++ Visualisations/Visualisations/loss.py
@@ -9,7 +9,7 @@ def loss(x,one_hot):
     counts=torch.zeros_like(indices)
     foundself=torch.zeros_like(indices)
     while not torch.all(foundself):
-        index[:]=indices[index]#### herea 
+        index=indices[index]#### herea 
         counts[torch.logical_not(foundself)]= counts[torch.logical_not(foundself)]+1
         foundself=torch.logical_or(foundself,indices[index]==torch.arange(indices.shape[0],device=indices.device))
     return torch.sum(x[xi,indices]*counts)
