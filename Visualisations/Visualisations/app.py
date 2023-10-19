06c79ab1de36fa0a3c5f6fb0130d2ad416f24e8b
steve <you@example.com>
2023-10-19 14:13:19 +0100

    fx

--- Visualisations/Visualisations/app.py
+++ Visualisations/Visualisations/app.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
 
         out={}
         # check if x is square i.e shape[0]==shape[1]
-        outputs={name:attempt(func,x) for name,func in functions.items()}
+        outputs={name:func(x) for name,func in functions.items()}
 
         #if x.shape[0]==x.shape[1]:
         losses=[(loss(outputs[name])*x[outputs[name].nonzero(as_tuple=True)]).tolist() for name,_ in functions.items()]
